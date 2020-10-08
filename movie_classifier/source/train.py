b7af99a41f36e73a058ab91a359e840c53a4b782
Hoang Duy <hdtrinh@MacBook-Air-di-Hoang.local>
2020-10-08 19:59:42 +0200

    Clean code

--- movie_classifier/source/train.py
+++ movie_classifier/source/train.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 # remove tf warnings
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 def warn(*args, **kwargs):
     pass
 import warnings
@@ -24,14 +24,19 @@ def main():
     df = df[pd.notnull(df.overview)]
     df = df[pd.notnull(df.title)]
     df = df[pd.notnull(df.genres)]
-    
+
     # Training parameters
     max_len_desc = 300
     max_len_title = 50
     max_input_len = max_len_title + max_len_desc
-    genres_to_be_predicted = ['Drama','Comedy','Documentary','Science Fiction','Romance']
-    num_classes = len(genres_to_be_predicted) 
-    
+    genres_to_be_predicted = [  'Drama',
+                                'Comedy',
+                                'Documentary',
+                                'Science Fiction',
+                                'Romance']
+                                
+    num_classes = len(genres_to_be_predicted)
+
     params = {
                 'GENRES':           genres_to_be_predicted,
                 'VOCABULARY_SIZE':  20000,
@@ -43,9 +48,9 @@ def main():
                 'NUM_CLASSES':      num_classes,
                 'NUM_EPOCHS':       4,
                 'BATCH_DIM':        64
-    
-    }   
-    
+
+    }
+
     # init custom classes
     p = preprocessor(genres = params['GENRES'])
     e = encoder(max_words = params['VOCABULARY_SIZE'],
@@ -56,7 +61,9 @@ def main():
     # prepare data for training
     df = p.preprocess(df)
     X, y = e.encode(df)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.30,
+                                                        random_state=1000)
     e.save()
 
     # create and train model
